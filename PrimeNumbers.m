4cd414c4c0da3c8405bb793d40fac409c7c7247b
19010853 <160854826+19010853@users.noreply.github.com>
2024-04-04 22:11:14 +0700

    Add files via upload

---
+++ PrimeNumbers.m
@@ -0,0 +1,40 @@
+function printPrimeNumber()
+  n = input("Nhap so nguyen n (n >= 1): ");
+
+  A = zeros(1, n);
+
+  for i = 1:n
+    A(i) = input(sprintf("Nhap phan tu A[%d]: ", i));
+  endfor
+
+  fprintf("");
+
+  for i = 1:n
+    if isprime(A(i))
+      fprintf("%d ", A(i));
+    endif
+  endfor
+
+  fprintf("\n");
+
+endfunction
+
+function result = isPrime(num)
+  if num < 2
+    result = false;
+    return;
+  endif
+
+  for i = 2:sqrt(num)
+    if num % i == 0
+      result - false;
+      return;
+    endif
+  endfor
+
+  result = true;
+
+endfunction
+
+printPrimeNumber();
+
